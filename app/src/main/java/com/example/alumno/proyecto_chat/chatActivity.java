13136ec8aaa7bec6bc99a7fa338a542410435dcf
Okrat3n <alvaromolina.alu@iespacomolla.es>
2017-10-04 11:58:33 +0200

    Nuevos cambios

--- app/src/main/java/com/example/alumno/proyecto_chat/chatActivity.java
+++ app/src/main/java/com/example/alumno/proyecto_chat/chatActivity.java
@@ -3,11 +3,11 @@ package com.example.alumno.proyecto_chat;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class chatActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_chat);
+        setContentView(R.layout.layout_chats);
     }
 }
