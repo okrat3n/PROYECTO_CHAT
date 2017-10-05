f76970f38ea52ae1ea57de37ab07db88f3029ac6
Okrat3n <alvaromolina.alu@iespacomolla.es>
2017-10-05 08:49:55 +0200

    empezando dinamico

--- app/src/main/java/com/example/alumno/proyecto_chat/chatActivity.java
+++ app/src/main/java/com/example/alumno/proyecto_chat/chatActivity.java
@@ -2,12 +2,37 @@ package com.example.alumno.proyecto_chat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class chatActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_chats);
+        setContentView(R.layout.layout_chat);
+
+        String[] conversacion = {"Hola,","que tal","bien y tu","bien tbm"};
+
+
+        TextView msg = null;
+        LinearLayout layout_mensaje = findViewById(R.id.layMessageContainer);
+        layout_mensaje.setOrientation(LinearLayout.VERTICAL);
+
+        for (int i = 0 ; i < conversacion.length ; i++)
+        {
+            msg = new TextView(this);
+            msg.setTextSize(27);
+            msg.setText(conversacion[i]);
+            layout_mensaje.addView(msg);
+        }
+
+
+
+
+
+
     }
 }
+
+
---
+++ app/src/main/java/mensaje.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+/**
+ * Created by alumno on 5/10/17.
+ */
+
+public class mensaje {
+
+    public String contenido;
+    public Date fecha_hora;
+    public String remitente;
+}
